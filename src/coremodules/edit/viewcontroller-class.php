685693b25346250feac64999e7a8d13331eab4a1
Patrick Puritscher <blackout289@gmail.com>
2015-03-03 23:57:34 +0100

    remove connections when post is deleted permanently

--- src/coremodules/edit/viewcontroller-class.php
+++ src/coremodules/edit/viewcontroller-class.php
@@ -31,9 +31,55 @@ class ViewController {
 		require_once( MUNECO_INCSPATH . 'functions.php' );
 
 		add_action( 'post_updated', array( $this, 'set_connections' ) );
+
+		add_action( 'delete_post', array( $this, 'unset_connections' ) );
 		$this->loadSubcontroller();
 	}
 
+	/**
+	 * Unsets connections at POST_delete
+	 * Still there when moved to trash, this is handled in edit/viewcontroller/subcontroller
+	 *
+	 * @param int $post_id
+	 *
+	 * @return bool
+	 */
+	public function unset_connections( $post_id ) {
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return $post_id;
+		}
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return $post_id;
+		}
+		$currentBlogID  = get_current_blog_id();
+		$currentSite    = new Site( $currentBlogID );
+		$currentNetwork = new Network();
+
+		if ( ! $currentSite->getMunecostatus() ) {
+			return $post_id;
+		}
+		/* Check if more than one site is has enabled muneco */
+		if ( count( $currentNetwork->getMunecoEnabledSites( true ) ) <= 1 ) {
+			return $post_id;
+		}
+		if ( ! isset( $_POST['post_connections_nonce'] ) ) {
+			return $post_id;
+		}
+		$nonce = $_POST['post_connections_nonce'];
+		if ( ! wp_verify_nonce( $nonce, MUNECO_BASENAME ) ) {
+			return $post_id;
+		}
+		$connections = array();
+		foreach ( $currentNetwork->getMunecoEnabledSites( true, array( $currentBlogID ) ) as $site ) {
+			if ( isset( $_POST[ 'post-connection-' . $site->blog_id ] ) ) {
+				$connections[ $site->blog_id ] = 0;
+			}
+		}
+		$currentJunction = new Junction( $currentBlogID, $post_id );
+		$currentJunction->setConnections( $connections );
+
+		return true;
+	}
 	/**
 	 * Sets connections at POST_update
 	 *
